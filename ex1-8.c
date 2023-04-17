19710258cc099bdd0a9b7b76625dbbdba4f4315a
Hyun-is <s7651111@gmail.com>
2023-04-17 14:04:30 +0900

    8

---
+++ ex1-8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*가우스의 덧셈으로 n까지의 정수 합 구하기*/
+
+/*int main (void)
+{
+    int n;
+    double sum = 0;
+
+    scanf("%d", &n);
+    
+    sum = (1 + (double)n) * ((double)n / 2);
+    
+    printf("%.0f\n", sum);
+    
+    return 0;
+    
+}*/
